b9c54023e36f2fa98b95a1594259da49f4bd4bfe
jesus nastasi <jesusnastasi@gmail.com>
2024-04-09 00:23:08 -0400

    agrego carpeta PAGES con html borrados anterior commit y agg archivo vite.config.js para configurar el BUILD y agregue los archivos html al build sin error

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,11 @@ export default {
     emptyOutDir: true,
     rollupOptions: {
       input: {
+
         index: resolve('index.html'),
-        mariosbross: resolve('mariosbross.html'),
-        mortalcombat: resolve('mortalcombat.html'),
-        pagoexitoso: resolve('pagoexitoso.html'),
+        mariosbross: resolve('pages/mariosbross.html'),
+        mortalcombat: resolve('pages/mortalcombat.html'),
+        pagoexitoso: resolve('pages/pagoexitoso.html'),
       }
     }
   }
